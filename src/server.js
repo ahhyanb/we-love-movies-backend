9dc307a6b3b3aa9a2ba3eaaecf788298cf40efb5
Ryan Bartolome <rahhyahh@icloud.com>
2024-11-16 23:58:47 -1000

    made sure knex runs migrations and then startst the server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
-const { PORT = 5002
- } = process.env;
-
+const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
+// Ensure knex runs migrations and then starts the server
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(PORT, listener);
+    app.listen(PORT, listener); // Bind to the PORT environment variable
   })
   .catch(console.error);
